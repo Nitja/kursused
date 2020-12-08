9cae1bb602158fc3c3c660679fd442be0970eed0
markzzzzzzz <markuslaidmets@hot.ee>
2020-12-08 13:25:47 +0200

    kustutasin git koera kaustast

---
+++ koerad/src/app/home/home.page.ts
@@ -0,0 +1,44 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
+})
+
+export class HomePage {
+  dogs = [
+    {
+      name: "lisa- mingi - nimi",
+      owner: "lisa - mingi - omanik",
+      age: "lisa - mingi - vanus",
+      picture: "lisa - mingi - pilt",
+    },
+    {
+      name: "lisa- mingi - nimi",
+      owner: "lisa - mingi - omanik",
+      age: "lisa - mingi - vanus",
+      picture: "lisa - mingi - pilt",
+    },
+    {
+      name: "lisa- mingi - nimi",
+      owner: "lisa - mingi - omanik",
+      age: "lisa - mingi - vanus",
+      picture: "lisa - mingi - pilt",
+    },
+    {
+      name: "lisa- mingi - nimi",
+      owner: "lisa - mingi - omanik",
+      age: "lisa - mingi - vanus",
+      picture: "lisa - mingi - pilt",
+    },
+    {
+      name: "lisa- mingi - nimi",
+      owner: "lisa - mingi - omanik",
+      age: "lisa - mingi - vanus",
+      picture: "lisa - mingi - pilt",
+    },
+  ];
+
+  constructor() {}
+}
