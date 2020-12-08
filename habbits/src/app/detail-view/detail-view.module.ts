d76715bec3b4140166d45273c42ed3ca8b6a0c64
markzzzzzzz <markuslaidmets@hot.ee>
2020-12-08 13:18:52 +0200

    detail view leht

---
+++ habbits/src/app/detail-view/detail-view.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { DetailViewPageRoutingModule } from './detail-view-routing.module';
+
+import { DetailViewPage } from './detail-view.page';
+
+//import { NgGitCalendarModule } from '../../../ng-git-calendar/src/lib/ng-git-calendar.module';
+//import { NgGitCalendarModule } from 'ng-git-calendar/lib/ng-git-calendar.module';
+import { NgGitCalendarModule } from '../../lib/ng-git-calendar.module';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    DetailViewPageRoutingModule,
+    NgGitCalendarModule  
+  ],
+  declarations: [DetailViewPage]
+})
+export class DetailViewPageModule {}
